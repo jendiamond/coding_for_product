9019dc27186926ae690a19514fcbf2ea679711d2
jendiamond <thejendiamond@gmail.com>
2017-06-02 16:26:47 -0700

    Add all Execises

---
+++ README.md
@@ -0,0 +1,6 @@
+## Solutions for the Coding for Product [Coding Exercises](http://codingforproduct.com/coding_exercise.html)
+These solutions are done in Ruby 2.3.0
+
+### Run them like this:
+#### $ `ruby codingforproduct_coding exercises.rb`
+#### $ `ruby codingforproduct_coding exercises_class.rb`
---
+++ codingforproduct_coding exercises.rb
@@ -0,0 +1,53 @@
+# Example 1 Jen Diamond ----------------------------------
+
+# http://codingforproduct.com/coding_exercise.html
+
+def truncate(text, cutoff_length = 50, string_suffix = "...")
+  text_length = cutoff_length - string_suffix.length
+  shortened_text = text[0..text_length+1]
+  shortened_text + string_suffix
+end
+
+def linked_title(given_hash)
+  full_title = given_hash[:title]
+  link = given_hash[:link]
+
+  if full_title.length >= 50
+    truncated_title = truncate(full_title)
+    '<a href="' + link + '">'  + truncated_title + '</a>'
+  else
+    '<a href="' + link + '">'  + full_title + '</a>'
+  end
+end
+
+puts "Example One:"
+puts linked_title({title: "really, really, really long title that will be chopped off", link: 'example.com'})
+
+# Example 2 Jen Diamond ----------------------------------
+
+EX2 = [
+  {
+    title: 'Github',
+    link: 'github.com'
+  },
+  {
+    title: 'Google',
+    link: 'google.com'
+  },
+  {
+    title: 'really, really, really long title that will be chopped off',
+    link: 'example.com'
+  }
+]
+
+def linked_titles(title_link_arr)
+  result=[]
+  titles_and_links = title_link_arr
+  titles_and_links.each do |x|
+    result << linked_title(x)
+  end
+  result
+end
+
+puts "Example Two:"
+puts linked_titles(EX2)
