9fbb9117ff52fa7495df343dc7eb15f9470076da
jendiamond <thejendiamond@gmail.com>
2017-06-06 09:58:44 -0700

    Update README to run Ruby

---
+++ GIVEN.rb
@@ -0,0 +1,81 @@
+EX3 = [
+  { title: "The Princess Bride: S. Morgenstern's Classic Tale of True Love and High Adventure",
+    link: 'https://en.wikipedia.org/wiki/The_Princess_Bride'
+  },
+
+  { title: "Longitude: The True Story of a Lone Genius Who Solved the Greatest Scientific Problem of His Time",
+    link: 'https://www.youtube.com/watch?v=gKVTmFtIHIc'
+  },
+
+  { title: "Your Flying Car Awaits: Robot Butlers, Lunar Vacations, and Other Dead-Wrong Predictions of the Twentieth Century",
+    link: 'https://www.harpercollins.com/9780061724602/your-flying-car-awaits'
+  },
+
+  { title: "The Ten, Make That Nine, Habits of Very Organized People. Make That Ten.: The Tweets of Steve Martin",
+    link: 'http://www.npr.org/books/titles/148593908/the-ten-make-that-nine-habits-of-very-organized-people-make-that-ten-the-tweets-'
+  },
+
+  { title: "Witches, Rakes, And Rogues: True Stories of Scam, Scandal, Murder, And Mayhem in Boston, 1630-1775",
+    link: 'http://www.goodreads.com/book/show/324878.Witches_Rakes_And_Rogues'
+  },
+
+  { title: "The Man Who Ate His Boots: The Tragic History of the Search for the Northwest Passage",
+    link: 'http://www.thenational.ae/arts-culture/books/the-man-who-ate-his-boots-the-doomed-quest-for-the-northwest-passage'
+  },
+
+  { title: "What Jane Austen Ate and Charles Dickens Knew: From Fox Hunting to Whist—the Facts of Daily Life in 19th-Century England",
+    link: 'https://www.publishersweekly.com/978-0-671-79337-1'
+  },
+
+  { title: "Why Do Men Have Nipples?: Hundreds of Questions You'd Only Ask a Doctor After Your Third Martini",
+    link: 'https://www.publishersweekly.com/978-1-4000-8231-5'
+  },
+
+  { title: "The Floating Brothel: The Extraordinary True Story of an Eighteenth-Century Ship and Its Cargo of Female Convicts",
+    link: 'https://www.publishersweekly.com/978-0-7868-6787-5'
+  },
+
+  { title: "When All Our Days Are Numbered Marching Bands Will Fill the Streets & We Will Not Hear Them Because We Will Be Upstairs in the Clouds (Novel(La))",
+    link: 'http://pankmagazine.com/2010/06/25/sasha-fletchers-when-all-our-days-are-numbered-marching-bands-will-fill-the-streets-we-will-not-hear-them-because-we-will-be-upstairs-in-the-clouds-a-review-by-troy-urquhart/'
+  },
+
+  { title: "Play the Piano Drunk Like a Percussion Instrument Until the Fingers Begin to Bleed a Bit",
+    link: 'http://www.goodreads.com/book/show/70885.Play_the_Piano_Drunk_Like_a_Percussion_Instrument_Until_the_Fingers_Begin_to_Bleed_a_Bit'
+  },
+
+  { title: "You Are Not So Smart: Why You Have Too Many Friends on Facebook, Why Your Memory Is Mostly Fiction, and 46 Other Ways You're Deluding Yourself",
+    link: 'http://backreaction.blogspot.com/2013/09/book-review-you-are-not-so-smart-by.html'
+  },
+
+  { title: "Noisy outlaws, unfriendly blobs, and some other things that aren't as scary, maybe, depending on how you feel about lost lands, stray cellphones, creatures from the sky, parents who disappear in Peru, a man named Lars Farf, and one other story [...]",
+    link: 'https://store.mcsweeneys.net/products/noisy-outlaws-unfriendly-blobs-and-some-other-things-dot-dot-dot'
+   },
+
+  { title: "Don't Get Too Comfortable: The Indignities of Coach Class, The Torments of Low Thread Count, The Never-Ending Quest for Artisanal Olive Oil, and Other First World Problems",
+    link: 'http://www.penguinrandomhouse.com/books/137441/dont-get-too-comfortable-by-david-rakoff/9780767916035/'
+   },
+
+  { title: "The Persecution and Assassination of Jean-Paul Marat as Performed by the Inmates of the Asylum of Charenton Under the Direction of the Marquis de Sade",
+    link: 'https://www.britannica.com/topic/Marat-Sade'
+  },
+
+  { title: "The Princess Bride: S. Morgenstern's Classic Tale of True Love and High Adventure",
+    link: 'https://en.wikipedia.org/wiki/The_Princess_Bride'
+  },
+
+  { title: "It Looked Different on the Model: Epic Tales of Impending Shame and Infamy",
+    link: 'http://www.penguinrandomhouse.com/books/122849/it-looked-different-on-the-model-by-laurie-notaro/9780345510990/'
+  },
+
+  { title: "The Physiology of Taste: Or, Meditations on Transcendental Gastronomy",
+    link: 'https://www.theguardian.com/books/2009/nov/22/physiology-of-taste-brillat-savarin'
+  },
+
+  { title: "Ophelia Joined the Group Maidens Who Don't Float: Classic Lit Signs on to Facebook",
+    link: 'https://books.google.com/books/about/Ophelia_Joined_the_Group_Maidens_Who_Don.html?id=OOHVy8meb94C'
+  },
+
+  { title: "The Disappearing Spoon: And Other True Tales of Madness, Love, and the History of the World from the Periodic Table of the Elements",
+    link: 'http://www.nytimes.com/2010/08/05/books/05book.html'
+  }
+]
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## Solutions for the Coding for Product [Coding Exercises](http://codingforproduct.com/coding_exercise.html)
 These solutions are done in Ruby 2.3.0
 
-### Run the code like this:
-#### $ `ruby codingforproduct_coding_exercises.rb`
-#### $ `ruby codingforproduct_class.rb`
+### Run the Ruby code
+#### coding_for_product$ `cd ruby`
+#### coding_for_product/ruby$ `ruby codingforproduct_coding_exercises.rb`
+#### coding_for_product/ruby$ `ruby codingforproduct_class.rb`
 
 ### Run the tests like this:
-#### $ `ruby test/codingforproduct_test.rb`
+#### coding_for_product/ruby$ `ruby test/codingforproduct_test.rb`
